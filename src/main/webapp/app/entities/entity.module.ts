980109ade426639c301372b7c23ceb2791e6bc73
dongnv <dongnv@itsol.vn>
2022-01-06 14:30:52 +0700

    add Area demo

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'area',
+        loadChildren: () => import('./area/area.module').then(m => m.GssgnAreaModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
---
+++ src/test/java/com/itsol/service/mapper/AreaMapperTest.java
@@ -0,0 +1,19 @@
+package com.itsol.service.mapper;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AreaMapperTest {
+
+    private AreaMapper areaMapper;
+
+    @BeforeEach
+    public void setUp() {
+        areaMapper = new AreaMapper();
+    }
+
+    @Test
+    public void testEntityFromId() {
+        Long id = 1L;
+    }
+}
